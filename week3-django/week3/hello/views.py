3a918377077d5fa96101cb2346edade770611bc4
GMiles44 <gmilescopeland@gmail.com>
2024-09-13 02:38:45 -0500

    Finished up the week 3 lecture, have not yet started but have downloaded the source code for the week 3 project: Wiki

--- week3-django/week3/hello/views.py
+++ week3-django/week3/hello/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "hello/index.html")
+
+def dave(request):
+    return HttpResponse("I'm afraid I can't let you do that, Dave")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
