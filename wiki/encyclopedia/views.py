4a23204aafd7db15b837069a6ab271eabb6aedf8
GMiles44 <gmilescopeland@gmail.com>
2024-09-16 01:05:15 -0500

    Working on project 'wiki', we've got the Entry and Index page specifications complete, next on the list is the Search function

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import markdown2
 from . import util
 
 
@@ -8,5 +8,13 @@ def index(request):
         "entries": util.list_entries()
     })
 
-def entry(request):
-    return render(request, "encyclopedia/entry.html")
+def entry(request, title):
+    try:
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "entry": markdown2.markdown(util.get_entry(title))
+        })
+    except TypeError:
+        return render(request, "encyclopedia/error.html", {
+            "error": "No entry exists, feel free to write one of your own!"
+        })
